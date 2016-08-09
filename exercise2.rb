271cb91961175a15acf914560d02aa7e95b2c5ec
CATHY CHAN <CATHYKY.CHAN@GMAIL.COM>
2016-08-09 14:02:23 -0400

    exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+#calculate tip
+tip = 55
+puts "$#{tip.to_i * 0.15}"
+
+#adding string and integer
+tip = 55
+puts "A good tip would be " + "$#{tip.to_i * 0.15}"
+
+#finding the product
+puts "#{45628*7839}"
+
+#value of expresssion
+puts (10 < 20 && 30 < 20) || !(10 == 11)
